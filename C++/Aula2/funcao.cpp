846cebc71b7654ef4928db2b049403267af1d349
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-16 18:32:34 -0300

    Create funcao.cpp

---
+++ C++/Aula2/funcao.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//função
+void calculaIdade()
+{
+	int anoA = 2020;
+	int anoN = 1999;
+	
+	int data = anoA - anoN;
+	
+	cout << data;
+}
+
+int main()
+{
+	//cabeçalho da função, chama a função
+	calculaIdade();	
+}
