5ef490d899efed7795ae0a645c86959bb1cbdc54
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-18 18:55:12 -0300

    Create estruturaVetor.cpp

---
+++ C++/Aula3/estruturaVetor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct coordenadas // nome da struct
+{
+	
+int x, y; //vareaveis que recebem o valor da vareavel prod
+
+} vet[] = {10, 20}; //vetor que adciona os valores as vareaveis x e y
+
+
+int main()
+{
+
+cout << vet[0].x << "\n"; 
+cout << vet[0].y;	
+}
+
+
+
