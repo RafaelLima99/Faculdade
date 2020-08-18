b68d4ea303b98af8533579ca13d9344f62946f55
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-18 18:55:47 -0300

    Create teste.cpp

---
+++ C++/Aula3/teste.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct data // nome da struct
+{
+	
+int dia, mes, ano; //vareaveis que recebem o valor da vareavel prod
+
+};
+
+int main()
+{
+
+ data a={25, 1, 20};
+ 	
+ cout << "dia: " << a.dia << "\n";
+ 
+}
+
+
+
