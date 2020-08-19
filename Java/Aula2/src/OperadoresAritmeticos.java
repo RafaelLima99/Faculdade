67f3b97a3919c144890fd946e2aaa975f61f3c70
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-19 17:08:40 -0300

    Create OperadoresAritmeticos.java

---
+++ Java/Aula2/src/OperadoresAritmeticos.java
@@ -0,0 +1,12 @@
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        
+        int result = num1 + num2;
+        
+        // para concatenar em java se usa o '+'
+        System.out.println("a soma é " + result);
+    }
+}
