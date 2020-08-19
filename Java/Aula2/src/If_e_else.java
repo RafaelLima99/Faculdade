dcba50c3f53d41997fc5ff81b902e052872125dc
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-19 17:08:32 -0300

    Create If_e_else.java

---
+++ Java/Aula2/src/If_e_else.java
@@ -0,0 +1,11 @@
+public class If_e_else {
+    public static void main(String[] args) {
+        int idade = 17;
+        
+        if(idade >= 18){
+            System.out.println("sua idade é " + idade + " voce é maior de idade");
+        }else{
+            System.out.println("sua idade é " + idade + " voce é menor de idade");
+        }
+    }
+}
