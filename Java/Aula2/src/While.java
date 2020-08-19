691d60f587723538f9ac59afe6dadfe515d4519c
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-19 17:08:52 -0300

    Create While.java

---
+++ Java/Aula2/src/While.java
@@ -0,0 +1,9 @@
+public class While {
+  public static void main(String[] args){
+      int contador = 0;
+      while(contador < 10){
+          contador ++;
+          System.out.println(contador);
+      }
+  }  
+}
