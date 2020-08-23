f0f8b7101a42e381fdd71cc502b13a60eb58228c
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-23 08:37:38 -0300

    Create Sobrecarga.java

---
+++ Java/Aula5/src/Sobrecarga.java
@@ -0,0 +1,12 @@
+public class Sobrecarga {
+    public Sobrecarga(String t, String c)
+    {
+        System.out.println(t+"\n");
+        System.out.println(c+"\n");
+    }
+    
+    public Sobrecarga(String a)
+    {
+        System.out.println(a);
+    }
+}
