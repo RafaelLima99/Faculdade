05faf3205c658ce3426bd9b70670b7ec2b3518f5
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-08-27 19:40:44 -0300

    Create Passageiro.java

---
+++ Java/Aula6/src/Passageiro.java
@@ -0,0 +1,14 @@
+public class Passageiro {
+
+    void embarque(Aviao aThis) {
+    }
+
+    void desembarque(Aviao aThis) {
+        
+    }
+
+    void desembarque(Aviao aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
