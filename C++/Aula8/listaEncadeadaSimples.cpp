26170224a1562c019fa8290601dd6d4dd35d2726
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-09-09 11:20:17 -0300

    Update listaEncadeadaSimples.cpp

--- C++/Aula8/listaEncadeadaSimples.cpp
+++ C++/Aula8/listaEncadeadaSimples.cpp
@@ -12,25 +12,35 @@ int main()
 {
 	system("color 0A");
 	//criando primeiro nó
-	//o primeiro no utiliza  a struct de forma normal
+	//o primeiro no utiliza a struct de forma normal
 	nodo *lista = new nodo;
+	//adciona 23 ao mebro num da strcut
 	lista->num = 23;
+	//adciona o valor do menbro prox como null
 	lista->prox=NULL;
 	
 	//criando o segundo nó
-	//o segundo nó ja usa o ponteiro prox pois nele esta armazenado o endereçoda strcut
-	//por isso acessamos a strcut depois o prox para depois acessar os mebros da struct que esta detro dele
-	
+	//o segundo nó ja usa a variavel ponteiro prox, pois nele esta armazenado o endereçoda strcut
+	//acessamos primeiro a strcut para depois acessar o menbro, prox para depois acessar os mebros da struct que esta detro de prox
 	lista->prox = new nodo;
 	lista->prox->num=13;
+	//acessando a strcut por meido da varivel prox  que tem o endereço da strcut
 	lista->prox->prox=NULL;
+	
+	//criando terceiro nó
+	lista->prox->prox = new nodo;
+	lista->prox->prox->num=14;
+	//acessando a strcut por meido da varivel prox  que tem o endereço da strcut
+	lista->prox->prox->prox = NULL;
 	
 	//exibindo
 	cout<<"\nValor do primeiro no: "<<lista->num;
 	//o valor do seundo no esta armazenado no proxy
 	cout<<"\nValor do segundo no: "<<lista->prox->num;
+	cout<<"\nValor do terceiro no: "<<lista->prox->prox->num;
 	cout<<"\n\nValor do endereco do primeiro no: "<<lista;
 	cout<<"\n\nValor do endereco do segundo no: "<<lista->prox;
+	cout<<"\n\nValor do endereco do terceiro no: "<<lista->prox->prox;
 	
 	//liberando
 	delete lista;
@@ -38,6 +48,7 @@ int main()
 	lista = 0;
 	cout<<"\nEndereco armazenado na varivael lista depois da atribuicao: "<<lista;
 	cout<<"\n\n";
+	
 	
 	
 }
