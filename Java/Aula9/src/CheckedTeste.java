8fcdb784f3729b2b3b488d48dfb01caab053caf2
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-09-13 17:48:28 -0300

    Create CheckedTeste.java

---
+++ Java/Aula9/src/CheckedTeste.java
@@ -0,0 +1,14 @@
+public class CheckedTeste {
+    //utliza o throw Exception, pq ele tbm foi usado no metodo deposita
+    //da class Checked
+    public static void main(String[] args) throws Exception{
+        Checked teste = new Checked();
+        
+        try{
+            teste.deposita(-1);
+        }catch(Exception e){
+            System.out.println("Não se pode depositar valor negativo");
+        }
+        System.out.println(teste.saldo);
+    }
+}
