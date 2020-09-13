fc840b76b3743fd0f5977faa6cd9a62e81c84f69
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-09-13 17:48:59 -0300

    Create Atividade.java

---
+++ Java/ClubeRecreativo/src/Atividade.java
@@ -0,0 +1,21 @@
+public class Atividade {
+    private int codAtv;
+    private String descricao;
+
+    public int getCodAtv() {
+        return codAtv;
+    }
+
+    public void setCodAtv(int codAtv) {
+        this.codAtv = codAtv;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
