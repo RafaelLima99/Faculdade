cc7a137d9ce01f4fd5f11a8d5a2fc10670fd9523
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-09-17 18:09:50 -0300

    Create TestaJOption.java

---
+++ Java/Testes/src/TestaJOption.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rafael lima
+ */
+public class TestaJOption {
+     
+    public static void main(String[] args){
+        JOption testa = new JOption();
+        testa.exibe();
+        
+    }
+}
+
