a95f75b4080aba7c26c50815ea74faa416e7ba96
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-09-17 18:08:32 -0300

    Create JOption.java

---
+++ Java/Testes/src/JOption.java
@@ -0,0 +1,8 @@
+
+import javax.swing.JOptionPane;
+
+public class JOption {
+    public void exibe(){
+        JOptionPane.showMessageDialog(null, "teste1", "teste2", JOptionPane.CANCEL_OPTION);
+    }
+}
