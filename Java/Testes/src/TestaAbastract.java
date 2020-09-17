f3026fb843f1af18312b3eb92408bdc5eecbed90
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-09-17 18:09:46 -0300

    Create TestaAbastract.java

---
+++ Java/Testes/src/TestaAbastract.java
@@ -0,0 +1,12 @@
+
+public class TestaAbastract {
+    public static void main(String[] args){
+    //utiliza o "{};" para implementar todos os metodos abtratos
+    //indica que é uma class abstrata
+    SubclassAbstrata testa = new SubclassAbstrata(){};
+    
+    testa.imprime();
+    
+    }
+    
+}
