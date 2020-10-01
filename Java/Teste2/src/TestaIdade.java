1426e1c8cc8057773fce9601bdbfda643a70d887
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-10-01 19:15:11 -0300

    Create TestaIdade.java

---
+++ Java/Teste2/src/TestaIdade.java
@@ -0,0 +1,10 @@
+public class TestaIdade {
+    public static void main(String[] args){
+        int dataA = 2020;
+        int dataN = 1999;
+        
+        Idade teste = new Idade(dataA, dataN);
+        
+        teste.exibeIdade();
+    }
+}
