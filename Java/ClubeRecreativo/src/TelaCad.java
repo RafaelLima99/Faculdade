2e1840e84f82a46e29d62af6a1a11e1e020ada54
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-10-01 19:14:43 -0300

    Update TelaCad.java

--- Java/ClubeRecreativo/src/TelaCad.java
+++ Java/ClubeRecreativo/src/TelaCad.java
@@ -33,6 +33,11 @@ public class TelaCad extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("Gravar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -61,6 +66,10 @@ public class TelaCad extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
