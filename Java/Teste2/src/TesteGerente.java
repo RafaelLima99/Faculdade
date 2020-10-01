0a6b212cfc1093149787e397fd5c260dfa7a3aac
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-10-01 19:15:16 -0300

    Create TesteGerente.java

---
+++ Java/Teste2/src/TesteGerente.java
@@ -0,0 +1,9 @@
+public class TesteGerente {
+    String nome = "Rafael";
+    String cargo = "Gerente";
+    int salario = 2000;
+    
+    Gerente g = new Gerente(nome, cargo, salario) {};
+    
+ 
+}
