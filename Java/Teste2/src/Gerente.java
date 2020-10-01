85df7ae6c51564f16c942cc6555a06536a958af1
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-10-01 19:14:57 -0300

    Create Gerente.java

---
+++ Java/Teste2/src/Gerente.java
@@ -0,0 +1,10 @@
+public abstract class Gerente extends Funcionario{
+    
+    public Gerente(String nome, String cargo, int salario) {
+        super(nome, cargo, salario);
+    }
+    
+    public void exibeSalario(){
+        
+    }
+}
