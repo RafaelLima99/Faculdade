329516eb9f01ca2f360f89ed281571df3a6ee3e8
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-10-01 19:15:07 -0300

    Create Mensage.java

---
+++ Java/Teste2/src/Mensage.java
@@ -0,0 +1,8 @@
+import java.awt.Component;
+import javax.swing.*;
+
+public class Mensage {
+    public static void main(String[] args){
+        JOptionPane.showInputDialog(null, "Conteudo", "titulo", JOptionPane.QUESTION_MESSAGE);
+    }
+}
